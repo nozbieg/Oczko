6c87c73b10d49d2d4ab5dfc0d16a2e9c08d97822
Norbert Olesiak-Zbieg <n.olesiak@gmail.com>
2021-05-09 15:20:04 +0200

    unit tests

---
+++ Core/Deck.cs
@@ -0,0 +1,33 @@
+﻿using Oczko.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oczko.Core
+{
+    public class Deck
+    {
+        public Deck()
+        {
+            GetCards();
+        }
+
+        public IList<Card> Cards { get; set; }
+
+        public IList<Card> GetCards()
+        {           
+            List<Card> cards = new List<Card>();
+            foreach(var type in (CardType[])Enum.GetValues(typeof(CardType)))
+            {
+                foreach (var colour in (CardColour[])Enum.GetValues(typeof(CardColour)))
+                {
+                    var card = new Card(type, colour);
+                    cards.Add(card);
+                }
+            }
+            return cards;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Oczko.Core;
+using System;
 
 namespace Oczko
 {
@@ -6,7 +7,7 @@ namespace Oczko
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var deck = new Deck();   
         }
     }
 }
