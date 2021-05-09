5257ec213e8755f6628c0ffb87f53289911c279a
Norbert Olesiak-Zbieg <n.olesiak@gmail.com>
2021-05-09 16:32:58 +0200

    Final

--- GameEngine/Game.cs
+++ GameEngine/Game.cs
@@ -39,6 +39,27 @@ namespace Oczko.GameEngine
             }
         }
 
+        public bool NextGameQuestion()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Play again? Y\\N");
+            var key =Console.ReadKey(true);
+            if (key.Key == ConsoleKey.Y)
+            {
+                return true;
+            }
+            else if (key.Key == ConsoleKey.N)
+            {
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Wrong key pressed. Select again");
+                Continue();
+                return false;
+            }
+        }
+
         public bool CheckEye(int actualValue)
         {
             if (actualValue <= 21)
@@ -99,6 +120,75 @@ namespace Oczko.GameEngine
         }
 
 
+        public void PlayGame(Game game)
+        {
+            Console.Clear();
+            var nextGame = false;
 
+            game.WelcomeScreen();
+            game.GetNextCard();
+            game.CountPlayerCardsValue(game.PlayerCards);
+            game.GetSummary(game.ActualValue, game.PlayerCards, game.Deck);
+
+            var play = game.Continue();
+            do
+            {
+                game.GetNextCard();
+                game.CountPlayerCardsValue(game.PlayerCards);
+                play = game.CheckEye(game.ActualValue);
+
+                if (play == true)
+                {
+                    game.GetSummary(game.ActualValue, game.PlayerCards, game.Deck);
+                    play = game.Continue();
+                    if(play == false)
+                    {
+                        game.CheckLastCard(game);
+                        nextGame = game.NextGameQuestion();
+                    }
+                }
+                else
+                {
+                    nextGame = game.NextGameQuestion();
+                }
+
+            } while (play == true);
+
+            if (nextGame == true)
+            {
+                game = new Game();
+                game.PlayGame(game);
+            }
+        }
+
+        private void CheckLastCard(Game game)
+        {
+            game.GetNextCard();
+            game.ActualValue += PlayerCards.LastOrDefault().CardValue;
+            var result = game.CheckLastEye(game.ActualValue, game);
+            if (result == false)
+            {
+                Console.WriteLine("Dealer got " + PlayerCards.LastOrDefault().ToString());
+
+                Console.WriteLine("His Value is " + game.ActualValue +" You lose");
+            }
+        }
+
+        private bool CheckLastEye(int actualValue, Game game)
+        {
+            if (actualValue <= 21)
+            {
+                return false;
+            }
+            else return Win(game);
+        }
+
+        private bool Win(Game game)
+        {
+            Console.WriteLine("Dealer got " + PlayerCards.LastOrDefault().ToString());
+            Console.WriteLine("His Value is " + game.ActualValue);
+            Console.WriteLine("Congratulation you won!");
+            return true;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,24 +10,7 @@ namespace Oczko
         {
             var game = new Game();
 
-            game.WelcomeScreen();         
-            game.GetNextCard();
-            game.CountPlayerCardsValue(game.PlayerCards);
-            game.GetSummary(game.ActualValue, game.PlayerCards, game.Deck);
-            var play = game.Continue();
-            do
-            {
-                game.GetNextCard();
-                game.CountPlayerCardsValue(game.PlayerCards);
-                play = game.CheckEye(game.ActualValue);
-                          
-                if(play == true)
-                {
-                    game.GetSummary(game.ActualValue, game.PlayerCards, game.Deck);
-                    play = game.Continue(); 
-                }
-                
-            } while (play == true);
+            game.PlayGame(game);
         }
     }
 }
